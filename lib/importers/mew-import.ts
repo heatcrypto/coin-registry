ec09a5fe193f70e9cda49428ab21e2cb0ea996ae
D.M. de Klerk <dmdeklerk@gmail.com>
2020-03-02 12:59:10 +0100

    Mew importer

--- lib/importers/mew-import.ts
+++ lib/importers/mew-import.ts
@@ -6,24 +6,26 @@
 import * as _fs from 'fs'
 const fs = _fs.promises
 import * as path from 'path'
-import { downloadJson } from '../utils/download';
+import { downloadJson, downloadFile } from '../utils/download';
 import { getDirs } from '../utils/get-dirs';
-const request = require('request');
-const rp = require('request-promise');
 const tokenListUrl = 'https://raw.githubusercontent.com/MyEtherWallet/ethereum-lists/master/dist/tokens/eth/tokens-eth.json'
 const registryDir = path.join(__dirname, '..', '..', 'registry')
 const ethereumTokenDir = path.join(registryDir, 'ethereum', '1')
 
 async function run() {
+  console.log(`Downloading MEW token list`)
   const mewJson = await downloadJson(tokenListUrl)
-  const tokenDirSetDualCase = await createTokenDirSetDualCase()
+  console.log(`Download complete, found ${mewJson.length} tokens`)
+  const tokenDirSet = await createTokenDirSet()
 
   for (let i = 0; i < mewJson.length; i++) {
     const { symbol, name, address, decimals } = mewJson[i]
 
     // Skip if address exists in registry
-    if (tokenDirSetDualCase.has(address))
+    if (tokenDirSet.has(address))
       continue;
+
+    console.log(`Found new token ${address} - ${name}`)
 
     // Create the token currency.json file
     const tokenDir = path.join(ethereumTokenDir, address)
@@ -34,29 +36,37 @@ async function run() {
       decimals,
     },null,2), 'utf-8')
 
+    // Download the logo from Trust/assets
+    const logoPngFile = path.join(tokenDir, 'logo.png')
+    const success:boolean = await downloadLogoFromTrustAssets(address, logoPngFile)
+    if (!success) 
+      console.log(`Could not download logo for ${address}`)
   }
 }
 
 /**
  * Creates a Set<string> of all token addresses found in the ethereum token registry.
- * In case an address uses mixed case we will create a lower case version as well and
- * also add this to the set.
- * 
  */
-async function createTokenDirSetDualCase() {
+async function createTokenDirSet() {
   const tokenDirs = await getDirs(ethereumTokenDir)
-  const tokenDirSetDualCase = new Set<string>()
+  const tokenDirSet = new Set<string>()
   tokenDirs.forEach(dirPath => {
     const address: string = path.basename(dirPath)
-    tokenDirSetDualCase.add(address)
-    const lowerCase = address.toLowerCase()
-    if (lowerCase != address) {
-      tokenDirSetDualCase.add(lowerCase)
-    }
+    tokenDirSet.add(address)
   })
-  return tokenDirSetDualCase
+  return tokenDirSet
 }
 
+/**
+ * Downloads raw png from github for ethereum token address 
+ * @param address 
+ * @param logoPngFile 
+ */
+async function downloadLogoFromTrustAssets(address:string, logoPngFile:string):Promise<boolean> {
+  const logoUrl = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+  await downloadFile(logoUrl, logoPngFile)
+  return _fs.existsSync(logoPngFile)
+}
 
 
 run();
